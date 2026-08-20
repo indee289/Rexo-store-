cb8918027f90a3ba33ce987504cb8ff9bf468646
indee289 <128210359+indee289@users.noreply.github.com>
2026-08-20 14:38:48 +0530

    Unresolved

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,3 @@
-export { PageHeader } from './PageHeader';
-export { EmptyState } from './EmptyState';
-export { FilterPill } from './FilterPill';
-
 export * from './PageHeader';
 export * from './EmptyState';
 export * from './FilterPill';
